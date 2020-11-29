47fc0482c2556820899ee0cf4ce00ae34dd6e3de
Ruijian Cao <caoruijian@big.one>
2020-11-29 10:53:28 +0800

    fix

--- cloudfunc/ping.go
+++ cloudfunc/ping.go
@@ -3,13 +3,14 @@ package cloudfunc
 
 import (
 	"fmt"
-	"github.com/sirupsen/logrus"
+	"log"
 	"net/http"
 	"time"
 )
 
 // Ping ping-pong test
 func Ping(w http.ResponseWriter, r *http.Request) {
-	logrus.Infof("%s request %s with agent: %s", r.Method, r.URL, r.UserAgent())
+	//logrus.Infof("%s request %s with agent: %s", r.Method, r.URL, r.UserAgent())
+	log.Printf("%s request %s with agent: %s", r.Method, r.URL, r.UserAgent())
 	fmt.Fprintf(w,"pong at %s", time.Now().Format(time.RFC3339Nano))
 }
